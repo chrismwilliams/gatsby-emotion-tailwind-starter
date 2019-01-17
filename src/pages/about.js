fd00e0ba734face746db9ee3136aca670d342b3c
Chris Williams <christophw+github@protonmail.com>
2019-01-17 15:16:07 +0000

    seo for about pg

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
 
-import { Layout } from '../components/elements';
+import { Layout, SEO } from '../components/elements';
 
 class AboutPg extends PureComponent {
   render() {
     return (
       <Layout>
+        <SEO title="About" />
         <h1>About Page</h1>
       </Layout>
     );
