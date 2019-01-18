fb72dcbf4a728edc0df61ed7fcd3d02f9b59087c
Chris Williams <christophw+github@protonmail.com>
2019-01-18 15:39:43 +0000

    export component elements for easy importing

--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -1,5 +1,8 @@
 import Layout from './layout';
 import Hero from './hero';
 import SEO from './seo';
+import ContentWrapper from './contentWrapper';
+import CardGrid from './cardGrid';
+import Card from './card';
 
-export { Layout, Hero, SEO };
+export { Layout, Hero, SEO, ContentWrapper, CardGrid, Card };
