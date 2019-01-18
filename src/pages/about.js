cc904462fb00bcb84613d2bdcb17d92a33041097
Chris Williams <christophw+github@protonmail.com>
2019-01-18 15:42:46 +0000

    add simple img with styling from card element

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,51 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from '@emotion/styled';
 
-import { Layout, SEO } from '../components/elements';
+import { Layout, SEO, ContentWrapper, Card } from '../components/elements';
 
-class AboutPg extends PureComponent {
-  render() {
-    return (
-      <Layout>
-        <SEO title="About" />
-        <h1>About Page</h1>
-      </Layout>
-    );
+const AboutContent = styled.section`
+  ${tw`mx-auto mt-12 max-w-xl`};
+
+  a {
+    ${tw`inline-block mt-4 ml-auto p-2 text-purple-dark text-sm font-semibold border-0 border-b border-dotted`};
   }
+`;
+
+export default function aboutPage({ data }) {
+  const { keyboardImg } = data;
+  return (
+    <Layout>
+      <SEO title="About" />
+      <ContentWrapper>
+        <h1>About Page</h1>
+        <AboutContent>
+          <Card>
+            <Img fluid={keyboardImg.childImageSharp.fluid} />
+            <a
+              href="https://unsplash.com/photos/feXpdV001o4"
+              rel="nofollow noopener noreferrer"
+              target="_blank"
+              aria-label="unsplash"
+            >
+              Photo by Anas Alshanti on Unsplash
+            </a>
+          </Card>
+        </AboutContent>
+      </ContentWrapper>
+    </Layout>
+  );
 }
 
-export default AboutPg;
+export const aboutQuery = graphql`
+  query {
+    keyboardImg: file(relativePath: { regex: "/keyboard.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 1080) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
