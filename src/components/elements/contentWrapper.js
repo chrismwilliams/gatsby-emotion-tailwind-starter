a7549e597004c7fc8166f3673832b698a3ba8856
Chris Williams <christophw+github@protonmail.com>
2019-01-19 13:22:07 +0000

    reduce padding on < md screen

--- src/components/elements/contentWrapper.js
+++ src/components/elements/contentWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const StyledWrapper = styled.div`
-  ${tw`max-w-4xl mx-auto px-8 py-16`};
+  ${tw`max-w-4xl mx-auto px-2 md:px-8 py-8 md:py-16`};
 `;
 
 export default function contentWrapper({ children }) {
