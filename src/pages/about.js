1d1b79ffb86aa272abed033cd15c243b8ed348bc
Chris Williams <christophw+github@protonmail.com>
2019-04-28 12:02:17 +0100

    rm setting link colour

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutContent = styled.section`
   ${tw`mx-auto mt-12 max-w-xl`};
 
   a {
-    ${tw`inline-block mt-4 ml-auto p-2 text-purple-dark text-sm font-semibold border-0 border-b border-dotted`};
+    ${tw`inline-block mt-4 ml-auto p-2 text-sm font-semibold border-0 border-b border-dotted`};
   }
 `;
 
