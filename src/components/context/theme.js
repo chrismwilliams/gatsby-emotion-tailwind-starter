b5588cf323d665d099da9c6818e5be2c0ac3209d
Chris Williams <christophw+github@protonmail.com>
2019-04-28 12:03:54 +0100

    get/set theme settings in localStorage

--- src/components/context/theme.js
+++ src/components/context/theme.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const ThemeContext = React.createContext();
 
 function ThemeProvider(props) {
-  const [theme, setTheme] = React.useState({ mode: 'white' });
+  const initialThemeSetting = () => {
+    const defaultTheme = { mode: 'light' };
+    if (typeof window !== 'undefined') {
+      return JSON.parse(window.localStorage.getItem('theme')) || defaultTheme;
+    }
+    return defaultTheme;
+  };
+  const [theme, setTheme] = React.useState(initialThemeSetting);
+
+  useEffect(() => {
+    window.localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
+
   const value = React.useMemo(() => [theme, setTheme], [theme]);
   return <ThemeContext.Provider value={value} {...props} />;
 }
