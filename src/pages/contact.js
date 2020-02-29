5fbcfae6812ebaafaa1c54b15951417024a3e398
Chris Williams <christophw+github@protonmail.com>
2020-02-29 09:22:42 +0000

    contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Layout, SEO } from '../components/elements';
+
+export default function Contact() {
+  return (
+    <Layout>
+      <SEO title="get in touch" />
+      <h1>get in touch</h1>
+    </Layout>
+  );
+}
