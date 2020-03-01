b500cd640fff65c68035ace431da2c0944965eea
Chris Williams <christophw+github@protonmail.com>
2020-03-01 11:16:44 +0000

    fix fouc on icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
 import 'typeface-open-sans';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 /*
-  Import Tailwind's base styles into the browser.
+Import Tailwind's base styles into the browser.
 */
 import './src/css/styles.css';
+
+config.autoAddCss = false;
