315c13774fea2ee670c735db60a60b2c4ba07ebd
Chris Williams <christophw+github@protonmail.com>
2020-05-11 16:22:29 +0100

    body styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,6 @@ import 'typeface-open-sans';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'tailwindcss/dist/base.css';
+import './src/css/styles.css';
 
 config.autoAddCss = false;
