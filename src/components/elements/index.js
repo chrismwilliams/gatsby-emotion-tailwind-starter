7098b0e91a5d3191cf3a3474b4cb0f3ce7836f41
Chris Williams <christophw+github@protonmail.com>
2020-05-11 16:25:12 +0100

    remove comp

--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -3,6 +3,5 @@ import Hero from './hero';
 import SEO from './seo';
 import CardGrid from './card-grid';
 import Card from './card';
-import Contact from './contact';
 
-export { Layout, Hero, SEO, CardGrid, Card, Contact };
+export { Layout, Hero, SEO, CardGrid, Card };
