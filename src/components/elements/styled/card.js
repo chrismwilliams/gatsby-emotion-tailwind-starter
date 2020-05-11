2bb15ec75cbaa10dd98bc5e7de885011bd2e25a0
Chris Williams <christophw+github@protonmail.com>
2020-05-11 16:28:36 +0100

    rm h3 styles

--- src/components/elements/styled/card.js
+++ src/components/elements/styled/card.js
@@ -2,9 +2,9 @@ import tw from 'twin.macro';
 import styled from '@emotion/styled';
 
 const StyledCard = styled.div`
-  ${tw`p-6 relative flex flex-col shadow-md`};
-  h3 {
-    ${tw`mb-5 text-2xl text-center`};
+  ${tw`relative flex flex-col overflow-hidden`};
+  .card-content {
+    ${tw`p-4`};
   }
 `;
 
