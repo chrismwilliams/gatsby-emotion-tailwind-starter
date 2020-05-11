a043218ea1c503b6d93d63b267783bf00bc56e72
Chris Williams <christophw+github@protonmail.com>
2020-05-11 16:24:42 +0100

    lib now import

--- src/components/elements/seo.js
+++ src/components/elements/seo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 
 export default function SEO({ title, description }) {
