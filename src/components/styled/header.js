7c1f9810ac70540148ad11cbaabd525770304ccc
Chris Williams <christophw+github@protonmail.com>
2020-05-11 16:30:56 +0100

    rm border, responsive padding

--- src/components/styled/header.js
+++ src/components/styled/header.js
@@ -2,32 +2,27 @@ import tw from 'twin.macro';
 import styled from '@emotion/styled';
 
 const StyledHeader = styled.header`
-  &::before {
-    ${tw`block w-full`};
-    content: '';
-    background: linear-gradient(
-      90deg,
-      #663399 0%,
-      #b721ff 20%,
-      #ffb238 60%,
-      #73fff7 95%
-    );
-    height: 5px;
-  }
+  ${tw`before:content`};
   nav {
+    ${tw`md:pl-12`};
     > ul {
-      ${tw`flex flex-wrap justify-center sm:justify-between`};
+      ${tw`flex flex-wrap items-center justify-center sm:justify-between`};
       li {
-        ${tw`flex-grow sm:flex-grow-0 text-xl text-center`};
+        ${tw`text-xl text-center`};
         &.logo {
-          ${tw`sm:mr-auto min-w-full sm:min-w-0`};
+          ${tw`mr-auto`};
           a {
-            ${tw`px-8 py-4 flex justify-center items-center`};
+            ${tw`flex items-center px-2 py-2 md:justify-center md:px-4`};
+            svg {
+              ${tw`block`};
+              width: 38px;
+              height: 68px;
+            }
           }
         }
         &:not(.logo) {
           a {
-            ${tw`block px-6 md:px-10 py-8`};
+            ${tw`block ml-4 p-3 md:ml-0 md:py-8 md:px-10`};
             &:hover {
               background: linear-gradient(
                   90deg,
