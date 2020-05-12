181fc963af64999b0380553ca36c8347c55a16d1
Chris Williams <christophw+github@protonmail.com>
2020-05-12 14:38:30 +0100

    style cards

--- src/components/elements/styled/card-grid.js
+++ src/components/elements/styled/card-grid.js
@@ -2,8 +2,45 @@ import tw from 'twin.macro';
 import styled from '@emotion/styled';
 
 const StyledCardGrid = styled.div`
-  ${tw`grid gap-6`};
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  ${tw`mt-32 md:px-16`};
+
+  h2,
+  p {
+    ${tw`px-2 md:text-center md:px-0`};
+  }
+
+  h2 {
+    ${tw`text-4xl font-bold text-gray-900`};
+  }
+
+  > p {
+    ${tw`mx-auto mt-4 text-xl font-light tracking-wider text-gray-600 md:text-center md:w-2/3`};
+  }
+
+  .cards {
+    ${tw`grid row-gap-10 col-gap-6 mt-16`};
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+
+    .card-wrapper {
+      ${tw`shadow-md md:rounded-lg hover:shadow-xl`};
+      .gatsby-image-wrapper {
+        ${tw`w-full`};
+        max-height: 450px;
+      }
+
+      .card-content {
+        .type {
+          ${tw`text-sm font-light text-indigo-500 capitalize`};
+        }
+        h3 {
+          ${tw`mt-3 text-xl font-semibold text-gray-800`};
+        }
+        p {
+          ${tw`mt-4 font-light tracking-wide text-gray-600`};
+        }
+      }
+    }
+  }
 `;
 
 export default StyledCardGrid;
